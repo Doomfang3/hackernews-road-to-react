bc40ee2777c3e0e2062d736d752dbbccdc7d2048
Mathieu Saubin <saubinm@free.fr>
2019-04-08 11:37:38 +0200

    complete second chapter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list,
+    }
+  }
+
+  onDismiss = id => {
+    const updatedList = this.state.list.filter(item =>  item.objectID !== id);
+    this.setState({ list: updatedList})
+  };
+
   render() {
-    const list = [
-      {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      }
-    ];
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
             <div key={item.objectID}>
               <a href={item.url} target="_blank" rel="noopener noreferrer"><h1>{item.title}</h1></a>
               <p>{item.author}</p>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)} type="button"> Dismiss
+                </button>
+              </span>
             </div>
         )}
       </div>
