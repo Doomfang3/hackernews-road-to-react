09babd8ca55df5513ce1f685dd85a7797103c6e5
Mathieu Saubin <saubinm@free.fr>
2019-04-10 10:30:13 +0200

    error handling

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends React.Component {
     this.state = {
       results: null,
       searchKey: "",
-      searchTerm: DEFAULT_QUERY
+      searchTerm: DEFAULT_QUERY,
+      error: null
     };
   }
 
@@ -53,7 +54,7 @@ class App extends React.Component {
     )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   };
 
   componentDidMount = () => {
@@ -85,7 +86,7 @@ class App extends React.Component {
   };
 
   render() {
-    const { searchTerm, results, searchKey } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0;
     const list =
@@ -102,7 +103,13 @@ class App extends React.Component {
             Search
           </Search>
         </div>
-        <Table list={list} onDismiss={this.onDismiss} />
+        {error ? (
+          <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+        ) : (
+          <Table list={list} onDismiss={this.onDismiss} />
+        )}
         <div className="interactions">
           <Button
             onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
